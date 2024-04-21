ba60b782a1d33e138371a318e493e26d92c80797
Ticlext-Altihaf <Ticlext-Altihaf@users.noreply.github.com>
2024-04-21 12:04:07 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -25,7 +25,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn(string $modelName) => 'BeraniDigitalID\\BeraniCommonFilamentPlugin\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
+            fn (string $modelName) => 'BeraniDigitalID\\BeraniCommonFilamentPlugin\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
         );
     }
 
